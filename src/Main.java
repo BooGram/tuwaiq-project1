406672968e225d8dd9217bab0b2551227146e7f4
dev <dev@test.com>
2026-04-10 00:00:12 +0300

    Create tic tac toe board and printBoard method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,31 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+        Random random = new Random();
+        char[][] board = {
+                {'1', '2', '3'},
+                {'4', '5', '6'},
+                {'7', '8', '9'}
+        };
+        printBoard(board);
 
+    }
 
+    public static void printBoard(char[][] board) {
+
+        System.out.println();
+
+        for (int i = 0; i < 3; i++) {
+
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("---------");
+        }
     }
 }
