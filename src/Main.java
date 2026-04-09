47975e997cd38b5452b242e3da2f6f72cdd69276
dev <dev@test.com>
2026-04-10 00:57:34 +0300

    feat: make it determine the winner

--- src/Main.java
+++ src/Main.java
@@ -83,6 +83,12 @@ public class Main {
 
             board[row][col] = player;
 
+            if (checkWin(board, player)) {
+                printBoard(board);
+                System.out.println("Player " + player + " Wins!");
+                break;
+            }
+
             if (player == 'X') {
                 player = 'O';
             }
@@ -105,5 +111,35 @@ public class Main {
             System.out.println();
             System.out.println("---------");
         }
+    }
+    public static boolean checkWin(char[][] board, char player) {
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == player &&
+                    board[i][1] == player &&
+                    board[i][2] == player)
+                return true;
+        }
+
+        for (int i = 0; i < 3; i++) {
+            if (board[0][i] == player &&
+                    board[1][i] == player &&
+                    board[2][i] == player)
+                return true;
+        }
+
+        if (board[0][0] == player &&
+                board[1][1] == player &&
+                board[2][2] == player)
+            return true;
+
+        if (board[0][2] == player &&
+                board[1][1] == player &&
+                board[2][0] == player)
+            return true;
+
+        return false;
+
+
     }
 }
